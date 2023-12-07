341ae1cad50f69f30572e6d1a170be7214acca46
Isira Adithya <rp.isira@gmail.com>
2023-12-07 20:53:30 +0530

    CustomerLogin API and AuthenticationFilter Filter class

--- src/main/java/com/isiraadithya/greensupermarket/filters/AuthenticationFilter.java
+++ src/main/java/com/isiraadithya/greensupermarket/filters/AuthenticationFilter.java
@@ -1,2 +1,36 @@
-package com.isiraadithya.greensupermarket.filters;public class AuthenticationFilter {
+package com.isiraadithya.greensupermarket.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+public class AuthenticationFilter implements Filter {
+    private String contextPath;
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        contextPath = filterConfig.getServletContext().getContextPath();
+        System.out.println("Context Path: " + contextPath);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+        String requestPath = req.getRequestURI();
+
+        if (req.getSession().getAttribute("isLoggedIn") != null){
+            if(requestPath.contains("admin/") && !requestPath.contains("/login")){
+                if (!req.getSession().getAttribute("role").equals("admin")){
+                    res.sendRedirect("/admin/login.jsp?err=Unauthorized");
+                }
+            }
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            if (requestPath.contains("/login")){
+                filterChain.doFilter(servletRequest, servletResponse);
+            } else {
+                res.sendRedirect("/login.jsp?err=Unauthorized");
+            }
+        }
+    }
 }
