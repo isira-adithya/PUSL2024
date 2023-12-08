e36297fa9c2403803237bc6ba52693c9794a8ce6
Isira Adithya <rp.isira@gmail.com>
2023-12-08 22:55:55 +0530

    Added Change Password Feature

---
+++ src/main/java/com/isiraadithya/greensupermarket/routes/user/UserChangePassword.java
@@ -0,0 +1,38 @@
+package com.isiraadithya.greensupermarket.routes.user;
+
+import com.isiraadithya.greensupermarket.models.User;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "userChangePassword", value = "/api/user/change-password")
+public class UserChangePassword extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String email = (String) req.getSession().getAttribute("email");
+        String current_password = req.getParameter("current_password");
+        String new_password = req.getParameter("new_password");
+
+//        Input Validation
+        if ((current_password == "null") || (new_password == "null") ){
+            resp.sendRedirect("/user/changepassword.jsp?err=Invalid Data");
+        }
+
+        User userObj = User.FindUserByEmail(email);
+        if (userObj.getUserId() != -1){
+            if (userObj.checkPassword(current_password)){
+                userObj.setPassword(new_password);
+                userObj.updateUser();
+                resp.sendRedirect("/user/profile.jsp?msg=Successful");
+            } else {
+                resp.sendRedirect("/user/changepassword.jsp?err=Invalid Password");
+            }
+        } else {
+            resp.sendRedirect("/user/changepassword.jsp?err=Invalid Data");
+        }
+    }
+}
