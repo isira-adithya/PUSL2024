9f5c27879fbc57844d2324f4a5ac95200771ff1c
Isira Adithya <rp.isira@gmail.com>
2023-12-08 19:06:49 +0530

    Added /api/user/delete route to delete user accounts

---
+++ src/main/java/com/isiraadithya/greensupermarket/routes/user/UserDelete.java
@@ -0,0 +1,26 @@
+package com.isiraadithya.greensupermarket.routes.user;
+
+import com.isiraadithya.greensupermarket.models.User;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "userDelete", value = "/api/user/delete")
+public class UserDelete extends HttpServlet {
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String email = (String) req.getSession().getAttribute("email");
+
+        User userObj = User.FindUserByEmail(email);
+        if (userObj.getUserId() != -1){
+            userObj.deleteUser();
+            resp.setStatus(200);
+        } else {
+            resp.sendRedirect("/user/profile.jsp?err=Invalid Data");
+        }
+    }
+}
