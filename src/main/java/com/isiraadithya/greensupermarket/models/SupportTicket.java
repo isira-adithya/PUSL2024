88829c45e8c72f9d30ea24f1ffea679efa449914
Isira Adithya <rp.isira@gmail.com>
2023-12-29 04:19:51 +0530

    Added a new function findSupportTicketById() to SupportTicket model

--- src/main/java/com/isiraadithya/greensupermarket/models/SupportTicket.java
+++ src/main/java/com/isiraadithya/greensupermarket/models/SupportTicket.java
@@ -129,6 +129,45 @@ public class SupportTicket {
         return supportTickets;
     }
 
+    public static SupportTicket findSupportTicketById(int ticketId){
+        try {
+            Connection dbconn = Database.connect();
+            String sqlQuery = "SELECT * FROM SupportTickets WHERE ticketid=?";
+            PreparedStatement sqlStatement = dbconn.prepareStatement(sqlQuery);
+            sqlStatement.setInt(1, ticketId);
+            ResultSet resultSet = sqlStatement.executeQuery();
+
+            int _ticketId = -1;
+            String _fullname = "NULL";
+            String _email = "NULL";
+            String _title = "NULL";
+            String _subject = "NULL";
+            boolean _markedAsRead = false;
+            Timestamp _createdAt = new Timestamp(1);
+            SupportTicket _tmp;
+            while(resultSet.next()){
+                _ticketId = resultSet.getInt("ticketid");
+                _fullname = resultSet.getString("full_name");
+                _email = resultSet.getString("email");
+                _title = resultSet.getString("title");
+                _subject = resultSet.getString("subject");
+                _markedAsRead = resultSet.getBoolean("markedAsRead");
+                _createdAt = resultSet.getTimestamp("createdAt");
+            }
+            _tmp = new SupportTicket(_fullname, _email, _title, _subject);
+            _tmp.setTicketId(_ticketId);
+            _tmp.setCreatedAt(_createdAt);
+            _tmp.setRead(_markedAsRead);
+
+            Database.closeConnection();
+
+            return  _tmp;
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return new SupportTicket("NULL","NULL","NULL","NULL");
+    }
+
     public void deleteTicket(){
         try {
             Connection dbconn = Database.connect();
