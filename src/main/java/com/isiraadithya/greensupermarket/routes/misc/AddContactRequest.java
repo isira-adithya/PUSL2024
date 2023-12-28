c03e58a5c0bdefca6a55eacd4733d2d0eb4aecfc
Isira Adithya <rp.isira@gmail.com>
2023-12-29 03:50:11 +0530

    Finished working on /api/misc/createSupportTicket API

--- src/main/java/com/isiraadithya/greensupermarket/routes/misc/AddContactRequest.java
+++ src/main/java/com/isiraadithya/greensupermarket/routes/misc/AddContactRequest.java
@@ -1,6 +1,7 @@
 package com.isiraadithya.greensupermarket.routes.misc;
 
 import com.isiraadithya.greensupermarket.models.Cart;
+import com.isiraadithya.greensupermarket.models.SupportTicket;
 import com.isiraadithya.greensupermarket.models.User;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -11,17 +12,18 @@ import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-@WebServlet(name = "userLogin", value = "/api/misc/addSupportTicket")
+@WebServlet(name = "createSupportTicket", value = "/api/misc/createSupportTicket")
 public class AddContactRequest extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
+            String name = req.getParameter("name");
             String email = req.getParameter("email");
-            String password = req.getParameter("password");
             String title = req.getParameter("title");
             String subject = req.getParameter("subject");
 
-
+            SupportTicket newTicket = new SupportTicket(name, email, title, subject);
+            newTicket.saveTicket();
 
             resp.sendRedirect("/contact-us.jsp?msg=Success");
         } catch (Exception ex){
